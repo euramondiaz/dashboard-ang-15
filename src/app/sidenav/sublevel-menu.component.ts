768264e2cc9c21be136918c361ed8db99aa682de
Ramon Dias <ramondiaz.ads@gmail.com>
2024-02-23 14:01:50 -0300

    update

--- src/app/sidenav/sublevel-menu.component.ts
+++ src/app/sidenav/sublevel-menu.component.ts
@@ -5,12 +5,28 @@ import { InavbarData } from './helper';
 @Component({
   selector: 'app-sublevel-menu',
   template: `
-    <p>
-      sublevel-menu works!
-    </p>
+  <ul *ngIf="collapsed && data.items && data.items.length> 0"
+  ngClass="sublevel-nav"
+  >
+    <li *ngFor="let item of data.items" class="sublevel-nav-item">
+      <a class="sublevel-nav-link"
+        *ngIf="item.items && item.items.length > 0"
+      >
+      <i class="sublevel-link-icon fa fa-circle"></i>
+      <span class="sublevel-link-text" *ngIf="collapsed">{{item.label}}</span>
+      <i *ngIf="item.items && collapsed" class="menu-collapse-icon"
+        [ngClass]="!item.expanded ? 'fal fa-angle-right' : 'fal fa-angle-down'"
+      ></i>
+      </a>
+      <a class="sublevel-nav-level">
+
+      </a>
+
+    </li>
+
+  </ul>
   `,
-  styles: [
-  ]
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SublevelMenuComponent implements OnInit {
 
@@ -24,7 +40,7 @@ export class SublevelMenuComponent implements OnInit {
   @Input() animating: boolean | undefined;
   @Input() expanded: boolean | undefined;
   @Input() multiple: boolean = false;
-  
+
 
 
 
